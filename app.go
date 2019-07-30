0f9e2ef0cc053c7279d0506293c9460057e25cf7
Alexandr Bezverkhniy <alexandr.bezverkhniy@usbank.com>
2019-07-30 11:54:43 -0500

    BIG Cleanup

--- app.go
+++ app.go
@@ -1,44 +1,13 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/alex-bezverkhniy/gonotes/repositories"
-
-	"github.com/alex-bezverkhniy/gonotes/controllers"
-
-	"github.com/alex-bezverkhniy/gonotes/routers"
-)
-
 // App - Application
 type App struct {
-	Port           string
-	DataFileName   string
-	Router         *routers.Router
-	NoteController *controllers.NoteController
+	Port         string
+	DataFileName string
 }
 
 // NewApp - create new instance
 func NewApp(port, dataFileName string) *App {
 	a := &App{Port: port, DataFileName: dataFileName}
-	noteRepository := repositories.NewNoteRepository(a.DataFileName)
-	a.NoteController = controllers.NewNoteController(noteRepository)
-	a.Router = routers.NewRouter(http.NewServeMux(), a.NoteController)
-
 	return a
-}
-
-// Serve - start http server
-func (a *App) Serve() {
-	log.Println("Go Notes Rest API v1.0")
-	if err := http.ListenAndServe(a.Port, a.Router.Mux); err != nil {
-		log.Fatal(err)
-	}
-}
-
-// Shutdown - stops http server
-func (a *App) Shutdown(code int) {
-	os.Exit(code)
 }
